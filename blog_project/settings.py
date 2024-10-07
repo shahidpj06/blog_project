8f57dbc704b848b4ef4420668ed158a72fd09c0d
shahidpj06 <shahidpj40651@gmail.com>
2024-10-07 16:02:34 +0530

    gitignore updated

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -133,11 +133,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+
 
 # Add this if you're using Django with Docker
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
